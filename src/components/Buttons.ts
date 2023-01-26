3e67b50026644136fc57d78533f3a84277545a19
EduardoRireyes <eduardorireyes2024@gmail.com>
2023-01-25 19:30:47 -0600

    Rediseño del header para mobile

--- src/components/Buttons.ts
+++ src/components/Buttons.ts
@@ -1,13 +1,65 @@
+ 
+export const buttonsAliveDead = document.createElement('div');
+buttonsAliveDead.id = 'buttonsAliveDead';
+buttonsAliveDead.className = 'header__buttonsAliveDead';
+////////////////////////////////////////////////////////////////
 
-export const Buttons = `
-    
-        <div id="buttonsOrder" class="buttonsOrder">
-            <button class="btnOrder" id="btnDefault">Default</button>
-            <button class="btnOrder" id="btnAlive">Alive</button>
-            <button class="btnOrder" id="btnDead">Dead</button>
-            <button class="btnOrder" id="btnAlphabetical">Alphabetical</button>
-            <button>Family White</button>
-            <button>Family Salamanca</button>
-        </div>
-`;
 
+const defaultBtn = document.createElement('button');
+defaultBtn.id = 'btnDefault';
+defaultBtn.className = 'btnOrder';
+defaultBtn.innerText = 'Default';
+
+const defaultBtnDiv = document.createElement('div');
+defaultBtnDiv.appendChild(defaultBtn);
+////////////////////////////////////////////////////////////////
+
+const aliveBtn = document.createElement('button');
+aliveBtn.id = 'btnAlive';
+aliveBtn.className = 'btnOrder';
+aliveBtn.innerText = 'Alive';
+
+const aliveBtnDiv = document.createElement('div');
+aliveBtnDiv.appendChild(aliveBtn);
+////////////////////////////////////////////////////////////////
+
+const deadBtn = document.createElement('button');
+deadBtn.id = 'btnDead';
+deadBtn.className = 'btnOrder';
+deadBtn.innerText = 'Dead';
+
+const deadBtnDiv = document.createElement('div');
+deadBtnDiv.appendChild(deadBtn);
+////////////////////////////////////////////////////////////////
+
+const alphaBtn = document.createElement('button');
+alphaBtn.id = 'btnAlpha';
+alphaBtn.className = 'btnOrder';
+alphaBtn.innerText = 'Alphabetical';
+
+const alphaBtnDiv = document.createElement('div');
+alphaBtnDiv.className = 'btnOrder-Alphabetic';
+alphaBtnDiv.appendChild(alphaBtn);
+////////////////////////////////////////////////////////////////
+
+const familyWhiteBtn = document.createElement('button');
+familyWhiteBtn.id = 'btnWhite';
+familyWhiteBtn.className = 'btnOrder';
+familyWhiteBtn.innerText = 'Family White';
+
+const familyWhiteBtnDiv = document.createElement('div');
+familyWhiteBtnDiv.className = 'btnOrder-FWhite';
+familyWhiteBtnDiv.appendChild(familyWhiteBtn);
+////////////////////////////////////////////////////////////////
+
+const familySalamancaBtn = document.createElement('button');
+familySalamancaBtn.id = 'btnSalamanca';
+familySalamancaBtn.className = 'btnOrder';
+familySalamancaBtn.innerText = 'Family Salamanca';
+
+const familySalamancaBtnDiv = document.createElement('div');
+familySalamancaBtnDiv.className = 'btnOrder-familySala';
+familySalamancaBtnDiv.appendChild(familySalamancaBtn);
+////////////////////////////////////////////////////////////////
+
+buttonsAliveDead.append(defaultBtnDiv, aliveBtnDiv, deadBtnDiv, alphaBtnDiv, familyWhiteBtnDiv, familySalamancaBtnDiv);
--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import './styles/footer.css'
 // import './styles/mediaQueries.css'
 import {CreateCharacter} from './classes/CreateCharacter'
 import dataCharacters from './utils/dataCharacters'
-import { HeaderBar } from './components/HeaderBar' 
-import { Buttons } from './components/Buttons';
+import { headerBackground, svgLogo, buttonOrderBy} from './components/Header' 
+import { buttonsAliveDead } from './components/Buttons';
 import {RenderCharacters} from './components/RenderCharacters'
 import {getIDs, getIDsAlphabetically, getIDsAlive, getIDsDead} from './utils/getData'
 import { Footer } from './components/Footer';
@@ -17,15 +17,10 @@ import {animationPage} from './utils/animationPage'
 //Renderiza la barra del Header
 export const renderHeader = document.querySelector('#headerBar') as HTMLElement;
 if(renderHeader != null){
-    renderHeader.innerHTML = HeaderBar;
+    renderHeader.innerHTML = svgLogo;
+    renderHeader.append(headerBackground, buttonOrderBy, buttonsAliveDead);
 }
 
-//Renderiza los Buttons
-export const inputButtons = document.querySelector('#buttons') as HTMLElement;
-if(inputButtons != null)
-{
-    inputButtons.innerHTML = Buttons;
-}
 
 //Renderiza las Cards
 const selector = document.querySelector('#sectionCards') as HTMLElement;
