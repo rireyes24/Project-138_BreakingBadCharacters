cb0670be7bef148a3b9a171d4304a146122178df
EduardoRireyes <eduardorireyes2024@gmail.com>
2023-01-29 14:59:55 -0600

    Cambio de base en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
-import {defineConfig} from 'vite'
+import {defineConfig, splitVendorChunkPlugin} from 'vite'
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
     server:{
         open: true,
         port: 1137,
     },
-    base: '/Project-138_BreakingBadCharacters/'
-});
+    //base: '/Project-138_BreakingBadCharacters/',
+    build: {
+        rollupOptions: {
+            plugins: [visualizer({
+                filename: 'stats.html',
+                open: true,
+                emitFile: true,
+            })],
+        }
+    },
+    plugins: [splitVendorChunkPlugin()],
+}); 
